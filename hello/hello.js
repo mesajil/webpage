ba1dff84617c1a18773d6901f29f20fa82c1aac6
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-10-19 15:07:47 -0500

    variable

--- hello/hello.js
+++ hello/hello.js
@@ -1,6 +1,9 @@
 
-console.log("Hello World!")
 
-document.write("<h1>Hello World!</h1>")
+alert("Hello!")
+let name = prompt("Enter your name")
+let age = Number(prompt("Enter your age"))
 
-alert("Hello World!")
+document.write("<h1>Title</h1>")
+document.write("Name: ", name)
+document.write("Age: ", age)
