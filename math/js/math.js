92021be3fd0d8a1234bd3dc75f4b15cffd00c3b7
Luis Hernán Mesajil López <lhmesajil@gmail.com>
2022-10-29 22:52:50 -0500

    square

--- math/js/math.js
+++ math/js/math.js
@@ -1,1 +1,8 @@
 
+const perimSquare = (side) => side * 4
+const areaSquare = (side) => side ** 2
+
+function calculatePerimSquare () {
+    let side = Number(document.getElementById ('side').value)
+    document.getElementById("main-output").innerText = perimSquare(side)
+}
