7be00a1da8e0b14c076809e2e0be675eaa7eb460
Luis Mesajil <lhmesajil@gmail.com>
2022-10-30 22:36:14 -0500

    result

--- math/js/math.js
+++ math/js/math.js
@@ -1,8 +1,31 @@
 
+const getSide = () => Number(document.getElementById ('side').value)
+const getHeight = () => Number(document.getElementById ('height').value)
+const getBase = () => Number(document.getElementById ('base').value)
+const getRadius = () => Number(document.getElementById ('radius').value)
 const perimSquare = (side) => side * 4
 const areaSquare = (side) => side ** 2
+const areaTriangle = (height, base) => height * base / 2
+const perimCircle = (radius) => 2 * Math.PI * radius
+const areaCircle = (radius) => Math.PI * (radius ** 2)
+
 
 function calculatePerimSquare () {
-    let side = Number(document.getElementById ('side').value)
-    document.getElementById("main-output").innerText = perimSquare(side)
+    document.getElementById("main-output").innerText = perimSquare(getSide())
+}
+
+function calculateAreaSquare () {
+    document.getElementById("main-output").innerText = areaSquare(getSide())
+}
+
+function calculateAreaTriangle () {
+    document.getElementById("main-output").innerText = areaTriangle(getHeight(), getBase())
+}
+
+function calculatePerimCircle () {
+    document.getElementById("main-output").innerText = perimCircle(getRadius())
+}
+
+function calculateAreaCircle () {
+    document.getElementById("main-output").innerText = areaCircle(getRadius())
 }
