ba28eccdb64410250e43bd5e39b399c25b6b47b3
Luis Mesajil <lhmesajil@gmail.com>
2022-11-01 18:59:54 -0500

    module

--- math/js/math.js
+++ math/js/math.js
@@ -1,31 +1,40 @@
+(() => {
+    'use strict'
 
-const getSide = () => Number(document.getElementById ('side').value)
-const getHeight = () => Number(document.getElementById ('height').value)
-const getBase = () => Number(document.getElementById ('base').value)
-const getRadius = () => Number(document.getElementById ('radius').value)
-const perimSquare = (side) => side * 4
-const areaSquare = (side) => side ** 2
-const areaTriangle = (height, base) => height * base / 2
-const perimCircle = (radius) => 2 * Math.PI * radius
-const areaCircle = (radius) => Math.PI * (radius ** 2)
+    const getOutput = document.getElementById("main-output"),
+        btnPerimSquare = document.getElementById('btnPerimSquare'),
+        btnAreaSquare = document.getElementById('btnAreaSquare'),
+        btnAreaTriangle = document.getElementById('btnAreaTriangle'),
+        btnPerimCircle = document.getElementById('btnPerimCircle'),
+        btnAreaCircle = document.getElementById('btnAreaCircle'),
+        getSide = () => Number(document.getElementById ('side').value),
+        getHeight = () => Number(document.getElementById ('height').value),
+        getBase = () => Number(document.getElementById ('base').value),
+        getRadius = () => Number(document.getElementById ('radius').value),
+        perimSquare = (side) => side * 4,
+        areaSquare = (side) => side ** 2,
+        areaTriangle = (height, base) => height * base / 2,
+        perimCircle = (radius) => 2 * Math.PI * radius,
+        areaCircle = (radius) => Math.PI * (radius ** 2)
+    
+    btnPerimSquare.addEventListener('click', () => {
+        getOutput.innerText = perimSquare(getSide())
+    })
+    
+    btnAreaSquare.addEventListener('click', () => {
+        getOutput.innerText = areaSquare(getSide())
+    })
+    
+    btnAreaTriangle.addEventListener('click', () => {
+        getOutput.innerText = areaTriangle(getHeight(), getBase())
+    })
+    
+    btnPerimCircle.addEventListener('click', () => {
+        getOutput.innerText = perimCircle(getRadius())
+    })
+    
+    btnAreaCircle.addEventListener('click', () => {
+        getOutput.innerText = areaCircle(getRadius())
+    })
 
-
-function calculatePerimSquare () {
-    document.getElementById("main-output").innerText = perimSquare(getSide())
-}
-
-function calculateAreaSquare () {
-    document.getElementById("main-output").innerText = areaSquare(getSide())
-}
-
-function calculateAreaTriangle () {
-    document.getElementById("main-output").innerText = areaTriangle(getHeight(), getBase())
-}
-
-function calculatePerimCircle () {
-    document.getElementById("main-output").innerText = perimCircle(getRadius())
-}
-
-function calculateAreaCircle () {
-    document.getElementById("main-output").innerText = areaCircle(getRadius())
-}
+})()
